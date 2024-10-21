1beee3f35839595df6dd9eea748ccfb8f7e6ae92
fiyotoli <tasfayneshtolasa35@gmail.com>
2024-10-21 20:45:13 +0300

    second  commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React_foodDelivery_App/', // Replace 'your-repo-name' with the name of your GitHub repository
+
 })
