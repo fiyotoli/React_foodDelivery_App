66126b76929e8cf178d37b40013fcf4d6ce64594
fiyotoli <tasfayneshtolasa35@gmail.com>
2024-10-21 22:28:14 +0300

    initial commit

--- src/pages/NoPage/NoPage.jsx
+++ src/pages/NoPage/NoPage.jsx
@@ -1,11 +1,23 @@
 // src/pages/NoPage.jsx
 import React from 'react';
+import { FaExclamationTriangle } from 'react-icons/fa'; // Importing an icon from react-icons
+import { useNavigate } from 'react-router-dom'; // Importing useNavigate for navigation
 
 const NoPage = () => {
+  const navigate = useNavigate(); // Create a navigate function
+
+  const handleBackHome = () => {
+    navigate('/'); // Navigate to the home page
+  };
+
   return (
-    <div className="text-center justify-content-center pt-5 mt-5">
-      <h2>404 Not Found</h2>
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100 text-center">
+      <FaExclamationTriangle className="display-1 text-danger" />
+      <h2 className='display-3 text-danger'>404 Not Found</h2>
       <p>The page you are looking for does not exist.</p>
+      <button className="btn btn-primary mt-3" onClick={handleBackHome}>
+        Back to Home
+      </button>
     </div>
   );
 };
