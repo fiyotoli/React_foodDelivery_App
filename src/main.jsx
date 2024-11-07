eeca53aab4ff62bfb73a1c54e110fb85712b2bbe
fiyotoli <tasfayneshtolasa35@gmail.com>
2024-11-07 13:44:12 +0300

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,30 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 // src/index.js or src/App.js
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS bundle
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/React_foodDelivery_App"> {/* Add basename here */}
-      <CartProvider>
-        <App />
-      </CartProvider>
+   <BrowserRouter>
+   <CartProvider> 
+   
+   <App/>
+   
+    </CartProvider>
+  
+ 
+      {/* App already has BrowserRouter */}
     </BrowserRouter>
   </StrictMode>,
-);
+)
